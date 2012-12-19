3f2c1b3130bd8df6eae110806ed4f5fed6962fe3
Damien Gallagher <damien.gallagher@nuim.ie>
2012-12-19 21:36:30 +0000

    another fix

--- app/models/dri/audio.rb
+++ app/models/dri/audio.rb
@@ -5,7 +5,7 @@ module DRI
   module Model
   class Audio < ActiveFedora::Base
     include Hydra::ModelMethods
-    include DRI::Metadata
+    # include DRI::Metadata
     #include Hydra::ModelMixins::CommonMetadata
  
     # These will need to be included to avoid deprecation warnings is later versions of HH
