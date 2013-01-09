2089dc6336705fe8eec5895ef7d363bb245f1236
Jimmy Tang <jtang@tchpc.tcd.ie>
2013-01-09 15:24:49 +0000

    microformatting

--- spec/models/audio_spec.rb
+++ spec/models/audio_spec.rb
@@ -31,12 +31,12 @@ describe DRI::Model::Audio do
     attributes_hash = {
       "title" => "An Audio Title",
       "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "presenter" => ["Collins, Michael"],
-    "guest" => ["DeValera, Eamonn", "Connolly, James"],
-    "broadcast_date" => "1916-04-01",
-    "subject" => ["Ireland","something else"],
-    "source" => ["CD nnn nuig"],
-    "language" => "ga"
+      "presenter" => ["Collins, Michael"],
+      "guest" => ["DeValera, Eamonn", "Connolly, James"],
+      "broadcast_date" => "1916-04-01",
+      "subject" => ["Ireland","something else"],
+      "source" => ["CD nnn nuig"],
+      "language" => "ga"
     }
     
     @audio.update_attributes( attributes_hash )
@@ -69,11 +69,11 @@ describe DRI::Model::Audio do
     attributes_hash = {
       "title" => "An Audio Title",
       "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    "presenter" => ["Collins, Michael"],
-    "guest" => ["DeValera, Eamonn", "Connolly, James"],
-    "broadcast_date" => "1916-04-01",
-    "subject" => ["Ireland","something else"],
-    "source" => ["CD nnn nuig"]
+      "presenter" => ["Collins, Michael"],
+      "guest" => ["DeValera, Eamonn", "Connolly, James"],
+      "broadcast_date" => "1916-04-01",
+      "subject" => ["Ireland","something else"],
+      "source" => ["CD nnn nuig"]
     }
 
     @audio.update_attributes( attributes_hash )
