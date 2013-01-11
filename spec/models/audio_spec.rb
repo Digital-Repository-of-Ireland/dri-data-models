67d04a7e5b6ce2d5713119dcb7304fb38693d293
Kathryn Cassidy <kcassidy@tchpc.tcd.ie>
2013-01-11 12:22:25 +0000

    refactoring to reduce duplicate code

--- spec/models/audio_spec.rb
+++ spec/models/audio_spec.rb
@@ -1,11 +1,28 @@
 # spec/modules/audio_spec.rb
 require 'spec_helper'
 
+
 describe DRI::Model::Audio do
   
   before(:each) do
     # This gives you a test article object that can be used in any of the tests
     @audio = DRI::Model::Audio.new
+
+    @attributes_hash = {
+      "title" => "An Audio Title",
+      "rights" => "This is a statement about the rights associated with this object",
+      "presenter" => ["Collins, Michael"],
+      "guest" => ["DeValera, Eamonn", "Connolly, James"],
+      "language" => "ga",
+      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      "broadcast_date" => "1916-04-01",
+      "creation_date" => "1916-01-01",
+      "source" => ["CD nnn nuig"],
+      "geographical_coverage" => ["Dublin"],
+      "temporal_coverage" => ["1900s"],
+      "subject" => ["Ireland","something else"]
+    }
+
   end
 
   it "should have the specified datastreams" do
@@ -28,22 +45,7 @@ describe DRI::Model::Audio do
   end
  
   it "should have the attributes of a audio and support update_attributes" do
-    attributes_hash = {
-      "title" => "An Audio Title",
-      "rights" => "This is a statement about the rights associated with this object",
-      "presenter" => ["Collins, Michael"],
-      "guest" => ["DeValera, Eamonn", "Connolly, James"],
-      "language" => "ga",
-      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "broadcast_date" => "1916-04-01",
-      "creation_date" => "1916-01-01",
-      "source" => ["CD nnn nuig"],
-      "geographical_coverage" => ["Dublin"],
-      "temporal_coverage" => ["1900s"],
-      "subject" => ["Ireland","something else"]
-    }
-    
-    @audio.update_attributes( attributes_hash )
+    @audio.update_attributes( @attributes_hash )
     
     # These attributes have been marked "unique" in the call to delegate, which causes the results to be singular
     @audio.title.class.to_s.should == 'String'
@@ -62,77 +64,43 @@ describe DRI::Model::Audio do
     @audio.temporal_coverage.class.to_s.should == 'Array'
 
     # The value should match what was set in the attributes_hash above
-    @audio.title.should == attributes_hash["title"]
-    @audio.rights.should == attributes_hash["rights"]
-    @audio.description.should == attributes_hash["description"]
-    @audio.broadcast_date.should == attributes_hash["broadcast_date"]
-    @audio.creation_date.should == attributes_hash["creation_date"]
-    @audio.language.should == attributes_hash["language"]
-    @audio.presenter.should == attributes_hash["presenter"]
-    @audio.guest.should == attributes_hash["guest"]    
-    @audio.subject.should == attributes_hash["subject"]
-    @audio.source.should == attributes_hash["source"]
-    @audio.geographical_coverage.should == attributes_hash["geographical_coverage"]
-    @audio.temporal_coverage.should == attributes_hash["temporal_coverage"]
+    @audio.title.should == @attributes_hash["title"]
+    @audio.rights.should == @attributes_hash["rights"]
+    @audio.description.should == @attributes_hash["description"]
+    @audio.broadcast_date.should == @attributes_hash["broadcast_date"]
+    @audio.creation_date.should == @attributes_hash["creation_date"]
+    @audio.language.should == @attributes_hash["language"]
+    @audio.presenter.should == @attributes_hash["presenter"]
+    @audio.guest.should == @attributes_hash["guest"]    
+    @audio.subject.should == @attributes_hash["subject"]
+    @audio.source.should == @attributes_hash["source"]
+    @audio.geographical_coverage.should == @attributes_hash["geographical_coverage"]
+    @audio.temporal_coverage.should == @attributes_hash["temporal_coverage"]
  
   end
 
   it "should automatically assign language=en where none is supplied" do
-    attributes_hash = {
-      "title" => "An Audio Title",
-      "rights" => "This is a statement about the rights associated with this object",
-      "presenter" => ["Collins, Michael"],
-      "guest" => ["DeValera, Eamonn", "Connolly, James"],
-      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "broadcast_date" => "1916-04-01",
-      "creation_date" => "1916-01-01",
-      "source" => ["CD nnn nuig"],
-      "geographical_coverage" => nil,
-      "temporal_coverage" => nil,
-      "subject" => ["Ireland","something else"]
-    }
+    @attributes_hash.delete("language")
 
-    @audio.update_attributes( attributes_hash )
+    @audio.update_attributes( @attributes_hash )
 
     @audio.language.should == "en"
 
   end
 
   it "should validate the presence of the title metadata field" do
-    attributes_hash = {
-      "rights" => "This is a statement about the rights associated with this object",
-      "presenter" => ["Collins, Michael"],
-      "guest" => ["DeValera, Eamonn", "Connolly, James"],
-      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "broadcast_date" => "1916-04-01",
-      "creation_date" => "1916-01-01",
-      "source" => ["CD nnn nuig"],
-      "geographical_coverage" => nil,
-      "temporal_coverage" => nil,
-      "subject" => ["Ireland","something else"]
-    }
+    @attributes_hash["title"] = ""
 
-    @audio.update_attributes( attributes_hash )
+    @audio.update_attributes( @attributes_hash )
 
     @audio.should_not be_valid
 
   end
 
   it "should validate the presence of the rights metadata field" do
-    attributes_hash = {
-      "title" => "An Audio Title",
-      "presenter" => ["Collins, Michael"],
-      "guest" => ["DeValera, Eamonn", "Connolly, James"],
-      "description" => "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-      "broadcast_date" => "1916-04-01",
-      "creation_date" => "1916-01-01",
-      "source" => ["CD nnn nuig"],
-      "geographical_coverage" => nil,
-      "temporal_coverage" => nil,
-      "subject" => ["Ireland","something else"]
-    }
+    @attributes_hash["rights"] = ""
 
-    @audio.update_attributes( attributes_hash )
+    @audio.update_attributes( @attributes_hash )
 
     @audio.should_not be_valid
 
