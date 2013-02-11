e71bf9a9db24190af6f56ea16a5f4ef1f6121918
Stuart Kenny <skenny@tchpc.tcd.ie>
2013-02-11 11:09:04 +0000

    Add collections to pdfdoc

--- app/models/dri/pdfdoc.rb
+++ app/models/dri/pdfdoc.rb
@@ -65,6 +65,20 @@ module DRI
     #
     def to_solr(solr_doc=Hash.new)
       super(solr_doc)
+
+      # Add title metadata from parent collections
+      collection_titles = []
+      if (governing_collection != nil)
+        collection_titles = [governing_collection.title]
+      end
+      collections.each do |coll|
+        collection_titles | coll.title
+      end
+      if (!collection_titles.empty?)
+        solr_doc.merge!(:collection_facet => collection_titles)
+        solr_doc.merge!(:collection_t => collection_titles)
+      end
+
       solr_doc.merge!(:object_type_facet => "Article")
       solr_doc
     end
