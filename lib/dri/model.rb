90990f8a94c49eb702cd9931593238c2b7275d0b
Damien Gallagher <damien.gallagher@nuim.ie>
2013-09-25 11:48:28 +0100

    Deleted all of DRI's old models. Rearranged a few files in lib/

--- lib/dri/model.rb
+++
@@ -1,7 +0,0 @@
-module DRI
-  module Model
-  	autoload :Batch, 'dri/batch'
-    autoload :GenericFile, 'dri/generic_file'
-    autoload :InterchangeableMetadata, 'dri/interchangeable_metadata'
-  end
-end
