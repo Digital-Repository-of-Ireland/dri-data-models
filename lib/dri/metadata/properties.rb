a807209788769f03c595b676016cb5c5d48d9aa5
Damien Gallagher <damien.gallagher@nuim.ie>
2013-09-25 15:25:24 +0100

    Corrected a bug in properties OM that didn't correctly save the object_type of the batch object.

--- lib/dri/metadata/properties.rb
+++ lib/dri/metadata/properties.rb
@@ -8,10 +8,10 @@ module DRI
       set_terminology do |t|
         t.root(:path=>"properties", :xmlns => '', :namespace_prefix => nil)  # Selects the root node of the XML document
         t.status(:namespace_prefix=>nil, :index_as=>[:symbol, :stored_searchable, :displayable, :facetable])
-        t.object_type(:path=>"properties/objectType", :namespace_prefix=>nil, :index_as=>[:displayable, :facetable])
+        t.object_type(:path=>"objectType", :namespace_prefix=>nil, :index_as=>[:displayable, :facetable])
         t.depositor(:namespace_prefix=>nil, :index_as=>[:stored_searchable, :displayable, :facetable])
         t.metadata_md5(:namespace_prefix=>nil, :index_as=>[:stored_searchable])
-        t.model_version(:path=>"properties/modelVersion", :namespace_prefix => nil)
+        t.model_version(:path=>"modelVersion",:namespace_prefix => nil)
         t.verified(:namespace_prefix=>nil, :index_as=>[:stored_searchable])
       end # set_terminology
 
