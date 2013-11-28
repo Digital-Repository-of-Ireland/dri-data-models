34d42b0a3436933e2955cf9181276fd5d919245a
Stuart Kenny <skenny@tchpc.tcd.ie>
2013-11-28 16:13:56 +0000

    Migrate to Active Fedora 6.7.0

---
+++ app/models/properties.rb
@@ -0,0 +1,18 @@
+module DRI
+  module ModelSupport
+  	module Properties
+      extend ActiveSupport::Concern
+
+      included do
+      	has_metadata :name => "properties", :type => DRI::Metadata::Properties
+
+        has_attributes :status, datastream: :properties, multiple: false
+        has_attributes :object_type, datastream: :properties, multiple: true
+        has_attributes :depositor, datastream: :properties, multiple: false
+        has_attributes :metadata_md5, datastream: :properties, multiple: false
+        has_attributes :model_version, datastream: :properties, multiple: false
+        has_attributes :verified, datastream: :properties, multiple: false
+      end
+    end
+  end
+end
