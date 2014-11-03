c39af36a22f75f5ccf133c8317841b87c9dce53c
Juliusz Filipowski <filipowj@tchpc.tcd.ie>
2014-11-03 10:29:56 +0000

    add author to marc model

--- lib/dri/metadata/marc.rb
+++ lib/dri/metadata/marc.rb
@@ -37,11 +37,12 @@ module DRI
         
         # common fields
         t.language(:path => 'record/datafield[@tag="041"]/subfield[@code="a"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.language_facetable])
-        t.publisher(:path => 'record/datafield[@tag="260"]/subfield[@code="b"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.cleaned_facetable])
+        t.publisher(:path => 'record/datafield[@tag="260"]/subfield[@code="b"] | //record/datafield[@tag="710"]/subfield[@code="x"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.cleaned_facetable])
         t.published_date(:path => 'record/datafield[@tag="260"]/subfield[@code="c"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.cleaned_facetable])
         t.author(:path => 'record/datafield[@tag="100" or @tag="110" or @tag="111"]/subfield[@code="a"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.cleaned_displayable, Descriptors.cleaned_facetable])       
         t.subject(:path => 'record/datafield[@tag="600" or @tag="610" or @tag="611" or @tag="630" or @tag="648" or @tag="650" or @tag="651" or @tag="653"]/subfield[@code="a"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.cleaned_facetable, Descriptors.cleaned_displayable])
         t.contributor(:path => 'record/datafield[@tag="700"]/subfield[@code="a"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.cleaned_displayable])
+        t.author(:path => 'record/datafield[@tag="100" or @tag="110" or @tag="111" or @tag="700"]/subfield[@code="a"]', :index_as=>[Descriptors.cleaned_searchable, Descriptors.cleaned_facetable, Descriptors.cleaned_displayable])
 
         # marc fields
         t.leader(:proxy => [:record, :leader])
