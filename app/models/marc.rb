289217082c47fc2411b78dc5299fa3814702bf4b
Stuart Kenny <skenny@tchpc.tcd.ie>
2014-11-04 11:55:57 +0000

    Support marc with multiple records

--- app/models/marc.rb
+++ app/models/marc.rb
@@ -13,6 +13,54 @@ class Marc < Batch
     descMetadata.type
   end
 
+  def update_metadata xml_text
+    if (xml_text.is_a? File)
+      xml_text = xml_text.read
+    end
+
+    objects = split_xml xml_text
+    descMetadata.ng_xml = objects[0]
+
+    objects[1..-1].each do |o|
+      new_object = Batch.with_standard :marc
+      new_object.governing_collection = governing_collection
+      new_object.depositor = depositor
+      new_object.status = status
+      new_object.update_metadata o
+      new_object.datastreams['rightsMetadata'].content = rightsMetadata.content
+
+      MetadataHelpers.checksum_metadata(new_object)
+
+      if new_object.valid?
+        new_object.save
+      end
+    end
+
+    return true
+  end
+
+  def split_xml xml_text
+    if (xml_text.is_a? Nokogiri::XML::Document)
+      xml = xml_text
+    else
+      xml = Nokogiri::XML xml_text
+    end
+  
+    collection = xml.search("//collection")
+    records = collection.children
+
+    objects = []
+    records.each do |r|
+      unless r.blank?
+        collection[0].children.remove
+        collection[0].add_child(r)
+        objects << collection[0].to_xml
+      end
+    end
+
+    return objects
+  end
+
   def attributes=(properties)
     controlfields = properties.delete('controlfield')
     datafields = properties.delete('datafield')
