9532c486734ed125ec8ed414340ab44dd842b9c5
Agustina Martinez <agustina.martinezgarcia@nuim.ie>
2015-05-21 15:10:10 +0100

    misc - fix for bug @gf.related_files method not found

--- app/models/dri/generic_file.rb
+++ app/models/dri/generic_file.rb
@@ -9,11 +9,16 @@ module DRI
     include Sufia::GenericFile::Permissions
     include Sufia::GenericFile::Derivatives
     #include Sufia::GenericFile::Trophies
+    include Sufia::GenericFile::Featured
     include Sufia::GenericFile::Metadata
-    include Sufia::GenericFile::Versions
     include Sufia::GenericFile::Content
+    include Sufia::GenericFile::Versions
     include Sufia::GenericFile::VirusCheck
     include Sufia::GenericFile::FullTextIndexing
+    include Sufia::GenericFile::ProxyDeposit
+    include Hydra::Collections::Collectible
+    include Sufia::GenericFile::Batches
+    include Sufia::GenericFile::Indexing
 
     before_destroy :delete_files
 
