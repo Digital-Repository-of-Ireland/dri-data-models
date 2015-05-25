076421e1926f4175c1fab8859e8f2f6ee999cc97
Agustina Martinez <agustina.martinezgarcia@nuim.ie>
2015-05-25 11:05:32 +0100

    misc - updated gf delete_files method

--- app/models/dri/generic_file.rb
+++ app/models/dri/generic_file.rb
@@ -90,7 +90,7 @@ module DRI
       FileUtils.remove_dir(Rails.root.join(Settings.dri.files).join(self.id), :force => true)
 
       storage = Storage::S3Interface.new
-      storage.delete_bucket(self.id)
+      storage.delete_bucket(Utils.split_id(self.id))
     end
   end
 end
