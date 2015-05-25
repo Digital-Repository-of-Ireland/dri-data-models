b177d145183fd158f5b35461a36230325fa5b1ed
Agustina Martinez <agustina.martinezgarcia@nuim.ie>
2015-05-25 12:46:31 +0100

    misc - updated delete_files callback

--- app/models/dri/generic_file.rb
+++ app/models/dri/generic_file.rb
@@ -76,6 +76,8 @@ module DRI
       solr_doc
     end
 
+    private
+
     def delete_files
       local_file_info = LocalFile.where("fedora_id LIKE :f AND ds_id LIKE :d",
                                         { :f => self.id, :d => 'content' }).order("version DESC").to_a
@@ -83,8 +85,7 @@ module DRI
       FileUtils.remove_dir(Rails.root.join(Settings.dri.files).join(self.id), :force => true)
 
       storage = Storage::S3Interface.new
-      storage.delete_bucket(self.id)
+      storage.delete_bucket(Utils.split_id(self.id))
     end
-
   end
 end
