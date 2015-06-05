e0451fe5730f57c5330eeb4c8b678eac7c2e7cec
Stuart Kenny <skenny@tchpc.tcd.ie>
2015-06-05 10:45:26 +0100

    buckets use batch id as name

--- app/models/dri/generic_file.rb
+++ app/models/dri/generic_file.rb
@@ -85,7 +85,7 @@ module DRI
       FileUtils.remove_dir(Rails.root.join(Settings.dri.files).join(self.id), :force => true)
 
       storage = Storage::S3Interface.new
-      storage.delete_bucket(self.id)
+      storage.delete_bucket(self.batch.id)
     end
   end
 end
