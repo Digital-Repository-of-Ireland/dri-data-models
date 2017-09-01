d4c8046b00595698bb5fd533315bfa4efd8787cc
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-09-01 21:29:02 +0100

    leave migrations in gem

--- lib/dri_data_models/engine.rb
+++ lib/dri_data_models/engine.rb
@@ -23,5 +23,13 @@ module DriDataModels
       g.assets false
       g.helper false
     end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
   end
 end
