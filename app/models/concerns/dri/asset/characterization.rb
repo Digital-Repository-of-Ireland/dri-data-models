b8926051e1c937b51394adf3561546a99ad7482f
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-09-15 14:12:47 +0100

    Save filename to characterization xml

--- app/models/concerns/dri/asset/characterization.rb
+++ app/models/concerns/dri/asset/characterization.rb
@@ -74,10 +74,10 @@ module DRI::Asset
 
       ## Extract the metadata from the content datastream and record it in the characterization datastream
       def characterize
-        metadata = content.extract_metadata
+        metadata = extract_metadata
         characterization.ng_xml = metadata if metadata.present?
         append_metadata
-        self.filename = [self.label]
+        characterization.filename = [self.label]
         save
       end
 
