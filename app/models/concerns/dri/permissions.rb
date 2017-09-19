c32e328f2d9538ee0561939c125f4c84131485a4
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-09-19 17:41:01 +0100

    Add permissions output for moab

--- app/models/concerns/dri/permissions.rb
+++ app/models/concerns/dri/permissions.rb
@@ -14,5 +14,32 @@ module DRI::Permissions
     return [] unless value
     value
   end
-  
+
+  def permissions
+    graph = RDF::Graph.new
+    %w(manager edit read discover).each do |permission|
+      permission_array("#{permission}_users").each do |entry|
+        graph << [ RDF::URI.new("http://projecthydra.org/ns/auth/person##{entry}"), mode(permission), "#{noid}" ]
+      end
+
+      permission_array("#{permission}_groups").each do |entry|
+        graph << [ RDF::URI.new("http://projecthydra.org/ns/auth/group##{entry}"), mode(permission), "#{noid}" ]
+      end
+    end
+
+    graph.dump(:ntriples)
+  end
+
+  def mode(permission)
+    case permission
+    when 'manager'
+      ::ACL.Control
+    when 'edit'
+      ::ACL.Write
+    when 'read'
+      ::ACL.Read
+    when 'discover'
+      Hydra::ACL.Discover
+    end
+  end
 end
