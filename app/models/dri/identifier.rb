36e0f814b3548f48d9e11b1bb805cd27767c976b
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-09-27 10:52:32 +0100

    Tidy up queries

--- app/models/dri/identifier.rb
+++ app/models/dri/identifier.rb
@@ -3,20 +3,18 @@ module DRI
     belongs_to :identifiable, polymorphic: true
 
     def self.retrieve_object(pid)
-      identifier = find_by(alternate_id: pid)
-      return nil unless identifier
-      identifier.identifiable
+      where(alternate_id: pid).take.try(:identifiable)
     end
 
     def self.retrieve_object!(pid)
-      identifier = find_by!(alternate_id: pid)
-      identifier.identifiable
+      object = retrieve_object(pid)
+      raise ActiveRecord::RecordNotFound.new("Couldn't find object with 'noid'=#{pid}") unless object
+
+      object
     end
 
     def self.object_exists?(pid)
-      identifier = find_by(alternate_id: pid)
-      return false unless identifier.try(:identifiable)
-      identifier.identifiable.class.exists?(identifier.identifiable_id)
+      !DRI::Identifier.find_by(alternate_id: pid).try(:identifiable).nil?
     end
 
     def self.gone?(pid)
