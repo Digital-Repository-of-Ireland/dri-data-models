c6ac815a8c8bbcbf6d7cbd0ba3644171218b60d2
Stuart Kenny <skenny@tchpc.tcd.ie>
2017-10-04 16:13:43 +0100

    call next_id directly in service

--- app/services/dri/noid.rb
+++ app/services/dri/noid.rb
@@ -7,7 +7,13 @@ module DRI
     ## This overrides the default behavior, which is to ask Fedora for an id
     # @see ActiveFedora::Persistence.assign_id
     def assign_id
-      service.mint
+      Mutex.new.synchronize do
+        loop do
+          pid = service.respond_to?(:minter) ? service.minter.send(:next_id) : service.next_id
+                    
+          return pid unless DRI::Identifier.exists?(alternate_id: pid)
+        end
+      end
     end
 
     def to_param
