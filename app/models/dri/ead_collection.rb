26d2ff4d282e2be468a51d91025a2ff9e2bac636
Stuart Kenny <skenny@tchpc.tcd.ie>
2021-02-04 18:43:49 +0000

    correct comment

--- app/models/dri/ead_collection.rb
+++ app/models/dri/ead_collection.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 # DRI namespace
 module DRI
-  # Implementation of DRI Marc digital objects
+  # Implementation of DRI EAD digital objects
   class EadCollection < DRI::DigitalObject
     include DRI::EncodedArchivalDescription
 
